c9c1c728dc93bb87e7bef14be17664036389b71b
GrandMasterX <zgrandmasterz@gmail.com>
2014-02-11 14:06:20 +0200

    trying to do native html5 drag & drop

--- app/assets/javascripts/carousel.js.coffee.js
+++ app/assets/javascripts/carousel.js.coffee.js
@@ -1,15 +1,59 @@
-jQuery ->
+$ ->
   $(".carousel").carousel({
   })
 
   $(".prev-slide").click ->
-  $("#myCarousel").carousel "prev"
+  	$("#myCarousel").carousel "prev"
   return
 
   $(".next-slide").click ->
-  $("#myCarousel").carousel "next"
+  	$("#myCarousel").carousel "next"
   return
 
+$(".content-item.music-video").draggable
+
 $ ->
-  $("#tracks,.widget-projection-widget").sortable(connectWith: ".widget-ourWork").disableSelection()
+	$(".widget-ourWork").droppable
+	  tolerance: "fit"
+	  accept: ".content-item.music-video"
+	  drop: (event, ui) ->
+	    $(this).append ui.draggable
+	    console.log this
+
+	    return
+$ ->
+	$(".counts a").on "click", ->
+  $that = this
+  $(this).parent().parent().find(".tracks").toggle "slow", ->
+    if $($that).text is "hide all"
+      $($that).text "show all"
+    else
+      $($that).text "hide all"
+    return
+
   return
+
+
+
+dragStart = (ev) ->
+  ev.dataTransfer.effectAllowed = "copy"
+  ev.dataTransfer.setData "Text", ev.target.getAttribute("id")
+  true
+
+
+dragEnter = (ev) ->
+  event.preventDefault()
+  true
+
+
+dragOver = (ev) ->
+  false
+
+
+dragDrop = (ev) ->
+  eleid = ev.dataTransfer.getData("Text")
+  ev.target.appendChild document.getElementById(eleid)
+  ev.preventDefault()
+  return
+
+
